db21771e8361a5979619bec811b8f339d07610e4
G. M. Mortuja <142245773+pgrmrmortuja@users.noreply.github.com>
2023-08-23 00:55:44 +0600

    Create UserInput.java

---
+++ UserInput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Dcoder {
+  
+    public static void main(String[] args) {
+      
+        String nam;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Your Name: ");
+        nam = input.nextLine();
+
+        input.close();
+
+        System.out.println("\n\nThanks " + nam);
+        
+    }
+}
