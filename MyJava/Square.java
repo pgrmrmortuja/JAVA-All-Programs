9d486b8a6a42b058195ae0c46e12a7cfd0bf9ac1
G. M. Mortuja <142245773+pgrmrmortuja@users.noreply.github.com>
2023-08-23 12:20:41 +0600

    Create Square.java

---
+++ MyJava/Square.java
@@ -0,0 +1,30 @@
+Package myjaba;
+
+import java.util.Scanner;
+
+class Dcoder {
+    public static void main(String[] args) {
+
+        String nam;
+        double s, area, parameter;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Your Name: ");
+        nam = input.nextLine();
+
+        System.out.print("Length of One Side: ");
+        s = input.nextDouble();
+
+        input.close();
+
+        area = s * s;
+        parameter = 4 * s;
+
+        System.out.println("Area of The Square: " + area);
+        System.out.println("And Parameter: " + parameter);
+
+        System.out.println("Thanks " + nam);
+        
+    }
+}
